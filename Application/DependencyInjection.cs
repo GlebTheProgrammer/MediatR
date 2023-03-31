e6795de2de8241dcfe3bda0796e9dfcbde63d4f4
Глеб Сукристик <Gleb15a@gmail.com>
2023-03-31 11:33:06 +0300

    Configured MediatR Clean Architecture template

---
+++ Application/DependencyInjection.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddAplication(this IServiceCollection services)
+        {
+            var assembly = typeof(DependencyInjection).Assembly;
+
+            services.AddMediatR(mediatrConfiguration =>
+                mediatrConfiguration.RegisterServicesFromAssembly(assembly));
+
+            services.AddValidatorsFromAssembly(assembly);
+
+            return services;
+        }
+    }
+}
---
+++ WebApi/Program.cs
@@ -0,0 +1,33 @@
+using Application;
+using Infrastructure;
+using Persistence;
+using Presentation;
+using Serilog;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services
+    .AddAplication()
+    .AddInfrastructure()
+    .AddPersistence()
+    .AddPresentation();
+
+builder.Host.UseSerilog((context, loggerConfiguration) => 
+    loggerConfiguration.ReadFrom.Configuration(context.Configuration));
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseSerilogRequestLogging();
+
+app.UseHttpsRedirection();
+
+app.Run();
